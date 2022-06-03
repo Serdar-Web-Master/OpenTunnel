5b2deec704ac4905d92d5f31e1559ce4bee401fc
opentmn <102440180+opentmn@users.noreply.github.com>
2022-06-03 11:51:55 +0500

    block2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const block1 = document.querySelector ('.block1');
 const block2 = document.querySelector ('.block2');
 
 setTimeout(f1,5000);
-setTimeout(f2,10000);
+// setTimeout(f2,10000);
 
 function f1 () {
     console.log("360px");
