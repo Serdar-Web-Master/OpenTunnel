741d613c20e9e416b7d29437c2eeb72cd3e77b3c
opentmn <102440180+opentmn@users.noreply.github.com>
2022-06-03 22:41:05 +0500

    a2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const block1 = document.querySelector ('.block1');
 const block2 = document.querySelector ('.block2');
+const key = document.querySelector ('.key');
 
 setTimeout(f1,5000);
 // setTimeout(f2,10000);
@@ -8,6 +9,8 @@ function f1 () {
     console.log("360px");
     block1.classList.add('move')
 }
+
+key.addEventListener("click",f2)
 function f2 () {
     console.log("720px");
     block2.classList.add('move')
